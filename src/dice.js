31e5f2024dc85ff44faff76075b45659cec9af0e
gwonhong <gwonhong.dev@gmail.com>
2021-05-25 06:54:22 +0900

    dice spin animation added! but too much spaces needed

--- src/dice.js
+++ src/dice.js
@@ -3,7 +3,8 @@ let diceHold = [];
 let rolled = 0;
 let diceVal = [];
 let rollButton = document.getElementById("rollButton");
-let chanceText= document.getElementById("chanceText");
+let chanceText = document.getElementById("chanceText");
+let dices = document.querySelectorAll('label');
 
 diceIcon[0] = document.getElementById("1d")
 diceIcon[1] = document.getElementById("2d")
@@ -23,45 +24,8 @@ function init() {
   for (let i = 0; i < 5; i++) {
     diceHold[i].disabled = true;
   }
-  chanceText.textContent="총 3번 굴릴 수 있습니다!";
+  chanceText.textContent = "총 3번 굴릴 수 있습니다!";
   rolled = 0;
-}
-
-function drawFace(n, i) {
-
-
-  switch (n) {
-    case 1:
-      diceIcon[i].setAttribute("class", "bi bi-dice-1-fill");
-      diceVal[i] = 1;
-      break;
-
-    case 2:
-      diceIcon[i].setAttribute("class", "bi bi-dice-2-fill");
-      diceVal[i] = 2;
-      break;
-
-    case 3:
-      diceIcon[i].setAttribute("class", "bi bi-dice-3-fill");
-      diceVal[i] = 3;
-      break;
-
-    case 4:
-      diceIcon[i].setAttribute("class", "bi bi-dice-4-fill");
-      diceVal[i] = 4;
-      break;
-
-    case 5:
-      diceIcon[i].setAttribute("class", "bi bi-dice-5-fill");
-      diceVal[i] = 5;
-      break;
-
-    case 6:
-      diceIcon[i].setAttribute("class", "bi bi-dice-6-fill");
-      diceVal[i] = 6;
-      break;
-  }
-
 }
 
 function throwDices() {
@@ -72,23 +36,30 @@ function throwDices() {
   for (let i = 0; i < 5; i++) {
     if (!diceHold[i].checked) {
       let randomNum = 1 + Math.floor(Math.random() * 6);
-      drawFace(randomNum, i);
+
+      /*drawface*/
+      diceIcon[i].setAttribute("class", "bi bi-dice-" + randomNum + "-fill rotate");
+      diceVal[i] = randomNum;
+      dices[i].classList.add('rotate');
     }
+    setTimeout(() => {
+      dices.forEach(element=>element.classList.remove('rotate'));
+    }, 800);
   }
+
   rolled++;
-  console.log("bn");
-  if(rolled===1){
-    chanceText.textContent="2번 남았습니다!";
+  if (rolled === 1) {
+    chanceText.textContent = "2번 남았습니다!";
   }
-  else if(rolled===2){
-    chanceText.textContent="1번 남았습니다!";
+  else if (rolled === 2) {
+    chanceText.textContent = "1번 남았습니다!";
   }
-  if (rolled > 2) {
+  else if (rolled === 3) {
     rollButton.disabled = true;
     for (let i = 0; i < 5; i++) {
       diceHold[i].setAttribute("disabled", "");
     }
-    chanceText.textContent="점수판에 클릭해서 기록해주세요!";
+    chanceText.textContent = "점수판에 클릭해서 기록해주세요!";
   }
   countDice();
   drawScore();
