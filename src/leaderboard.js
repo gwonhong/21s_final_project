6f286a410889c9c8d65ea5d373db9218a2173430
gwonhong <gwonhong.dev@gmail.com>
2021-05-25 04:08:16 +0900

    leaderboard first added and some minor fixes to index and board

---
+++ src/leaderboard.js
@@ -0,0 +1,16 @@
+let scores=[];
+function loadScores(){
+  let lastScores = localStorage.getItem("scores");
+  scores=JSON.parse(lastScores);
+  scores.forEach();
+}
+function saveScores(){
+
+}
+
+function scoreToBoard(){
+  let score ={
+    time: time,
+    score: score
+  };
+}
